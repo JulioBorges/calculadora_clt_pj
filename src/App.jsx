a120f27aca99207e5893654867b8fd4df6550aa6
Julio Borges <julio.borges@take.net>
2023-12-12 13:54:25 -0300

    correct SpeedInsights library

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import './App.css';
 
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import CltSalary from './components/CltSalary';
 import Footer from './components/Footer';
 import Header from './components/Header';
